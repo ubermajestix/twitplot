25567fadb4963d61308820ec7449df03b80c0d79
Corey Donohoe <atmos@atmos.org>
2009-02-15 14:22:59 -0700

    first steps towards webrat testing for twitplot

---
+++ test/integration/visiting_twitplot_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__)+'/../test_helper')
+
+class VisitingTwitplotTest < ActionController::IntegrationTest
+  # Replace this with your real tests.
+  test "the truth" do
+    visit '/'
+    assert_have_selector 'div#about'
+    assert_have_selector 'div#results'
+    assert_have_selector "form[action='/search']"
+    assert_have_selector "form input#location[name='location'][type='text'][value='Boulder, Co']"
+    assert_have_selector "form input#keyword[name='keyword'][type='text']"
+    assert_have_selector "form select#distance[name='distance']"
+    [10,25,50].each do |miles|
+      assert_have_selector "form select#distance option[value='#{miles}']:contains('#{miles}')"
+    end
+
+    assert_have_selector "form input[type='submit'][name='commit'][value='search']"
+
+    click_button 'search'
+    puts response_body
+  end
+end
